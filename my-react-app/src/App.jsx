fec98620559ec8ddd46aaed3a5d4613add31eb3b
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-07-17 22:30:29 +0530

    landing page skeleton

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Landing from "./pages/Landing";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div></div>
+      <div>
+        <Landing />
+      </div>
     </>
   );
 }
---
+++ my-react-app/src/pages/Landing.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import styled from "styled-components";
+
+export default function Landing() {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info*/}
+        <div className="info">
+          <h1>
+            {" "}
+            job <span>tracking</span> app{" "}
+          </h1>
+        </div>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s
+        </p>
+        <button className="btn btn-hero">Login/Register</button>
+      </div>
+      <img className="img main-img" src={main} />
+    </Wrapper>
+  );
+}
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
