16b28de30aaaa721e3c29b64fc252ff18a05d84f
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-07-19 22:05:02 +0530

    browserRouter

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { Landing, Dashboard, Error, Register } from "./pages";
+import {
+  Profile,
+  AddJob,
+  AllJobs,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard";
+import { Landing, Error, Register } from "./pages";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
@@ -8,7 +14,13 @@ function App() {
     <BrowserRouter>
       {/* <Landing /> */}
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/" element={<Dashboard />} /> */}
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
