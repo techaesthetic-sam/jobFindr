c792da8ad70c0a08ac29e48f8aa7e7b4a1497e27
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-07-21 23:12:04 +0530

    big sidebar

--- my-react-app/src/components/BigSideBar.jsx
+++ my-react-app/src/components/BigSideBar.jsx
@@ -1,5 +1,28 @@
-import React from "react";
-import Wrapper from "../wrappers/BigSidebar";
-export default function BigSideBar() {
-  return <Wrapper>BigSideBar</Wrapper>;
-}
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
+import Wrapper from "../assets/wrappers/BigSidebar";
+import { useSelector } from "react-redux";
+
+const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default BigSidebar;
