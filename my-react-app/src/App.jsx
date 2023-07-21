3fdc97bb25362010989ab4bf78e5dcfb092a4dc6
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-07-21 22:40:26 +0530

    added protectedRoute

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import {
   Stats,
   SharedLayout,
 } from "./pages/dashboard";
-import { Landing, Error, Register } from "./pages";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
@@ -15,8 +15,16 @@ function App() {
       {/* <Landing /> */}
       <Routes>
         {/* <Route path="/" element={<Dashboard />} /> */}
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
+          {/* Home page context - index , SharedLayout common for all nested */}
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
           <Route path="profile" element={<Profile />} />
